9abddbbf4ae33e60daf4bbdac21cc0e2b2448e42
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-05-24 16:06:21 -0400

    More to do

--- controllers/getData.js
+++ controllers/getData.js
@@ -1,5 +1,6 @@
 const data = require('../models/products')
 
+//Remember that in the models file I have the data stored in variable 'products', so this is returning the variable of the file 'data' with the specific data called 'products' so data.products is returned
 module.exports.getProducts = () => {
     return data.products
 }
--- server.js
+++ server.js
@@ -4,27 +4,50 @@ const app = express()
 
 //SETUP CONTROLLER AND CALL PRODUCTS FUNCTION
 const dataController = require('./controllers/getData')
-const { products } = require('./models/products')
 const productData = dataController.getProducts()
 
-//PORT CHANNEL
-const PORT = 3000
+// Middleware is a function
+//they update the request as soon as they come in.
+app.use((req, res, next) => {
+    console.log('Middleware running')
+    next() // go to the next middleware or to the response
+})
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 //SETUP VIEW ENGINE
 app.set('view engine', 'ejs')
 app.set('views', './views')
+
+//PORT CHANNEL
+const PORT = 3000
 
 //READ ROOT ROUTE AND RENDER ITS INDEX EJS FILE
 app.get('/', (req, res) => {
     res.render('index', { pageTitle: "Home", header: 'Welcome Consumer' })
 })
 
-//SETUP OTHER ROUTES: PRODUCTS AND NEW
-app.get('/products',(req,res)=>{
-    res.render('products')
+//SETUP PRODUCTS ROUTE 
+app.get('/products', (req, res) => {
+    res.render('products',{data: productData})
+    res.send(productData)
 })
-app.get('/new', (req,res) => {
+//SETUP NEW ROUTE
+app.get('/products/new', (req, res) => {
     res.render('new')
+})
+
+//REMEMBER SPECIFICITY OF ROUTE. This should be below general routes
+//SETUP PRODUCT ID ROUTE
+app.get('/products/:id',(req,res)=>{
+    res.render('products',{data: productData})
+})
+
+
+app.post('/products',(req,res)=>{
+console.log(req.body)
+productData.push(req.body)
+res.redirect('products')
 })
 
 //LISTEN TO PORT
